37669bea2c6a68d2363ed6bb0ee7580e86d4e82c
Molekula <tomas@dlabac.net>
2024-09-27 20:08:25 +0200

    16kb Mapper

--- rtl/peripheral/slots/mappers.sv
+++ rtl/peripheral/slots/mappers.sv
@@ -4,7 +4,7 @@ module mappers (
     block_info          block_info,    // Struct containing block configuration and parameters
     device_bus          device_bus,    // Interface for device control
     memory_bus          memory_bus,    // Interface for memory control
-    output       [7:0]  data,           // Data output from the active mapper; defaults to FF if no mapper is active
+    output       [7:0]  data,          // Data output from the active mapper; defaults to FF if no mapper is active
     input        [7:0]  data_to_mapper
 );
 
@@ -18,6 +18,7 @@ module mappers (
     mapper_out konami_SCC_out();        // Outputs from KONAMI SCC mapper
     mapper_out gm2_out();               // Outputs from Konami GameMaster mapper
     mapper_out msx2_ram_out();          // Outputs from MSX2_RAM mapper
+    mapper_out m_16kb_out();            // Outputs from 16kb mapper
     device_bus fm_pac_device_out();     // Device bus output for FM-PAC mapper
     device_bus konami_SCC_device_out(); // Device bus output for SCC mapper
     device_bus offset_device_out();     // Device bus output for offset mapper (default mapper)
@@ -85,18 +86,25 @@ module mappers (
         .data_to_mapper(data_to_mapper)
     );
     
+    // Instantiate the 16kb mapper
+    mapper_16kb mapper_16kb (
+        .cpu_bus(cpu_bus),
+        .block_info(block_info),
+        .out(m_16kb_out)        
+    );
+    
     // Data: Use the FM-PAC mapper's data output, assuming it has priority
     assign data = fm_pac_out.data;  // FM-PAC mapper has priority for data output
 
     // Combine outputs from the mappers
     // Address: Combine addresses from all mappers using a bitwise AND operation
-    assign memory_bus.addr  = ascii8_out.addr & ascii16_out.addr & offset_out.addr & fm_pac_out.addr & konami_out.addr & gm2_out.addr & konami_SCC_out.addr & msx2_ram_out.addr;
+    assign memory_bus.addr  = ascii8_out.addr & ascii16_out.addr & offset_out.addr & fm_pac_out.addr & konami_out.addr & gm2_out.addr & konami_SCC_out.addr & msx2_ram_out.addr & m_16kb_out.addr;
 
     // Read/Write control: Combine read/write signals from all mappers using a bitwise AND operation
     assign memory_bus.rnw   = ascii8_out.rnw & ascii16_out.rnw & offset_out.rnw & fm_pac_out.rnw & gm2_out.rnw & msx2_ram_out.rnw;
 
     // RAM chip select: Combine RAM chip select signals using a bitwise OR operation
-    assign memory_bus.ram_cs    = ascii8_out.ram_cs | ascii16_out.ram_cs | offset_out.ram_cs | fm_pac_out.ram_cs | konami_out.ram_cs | gm2_out.ram_cs | konami_SCC_out.ram_cs | msx2_ram_out.ram_cs;
+    assign memory_bus.ram_cs    = ascii8_out.ram_cs | ascii16_out.ram_cs | offset_out.ram_cs | fm_pac_out.ram_cs | konami_out.ram_cs | gm2_out.ram_cs | konami_SCC_out.ram_cs | msx2_ram_out.ram_cs | m_16kb_out.ram_cs;
 
     // SRAM chip select: Combine SRAM chip select signals using a bitwise OR operation
     assign memory_bus.sram_cs   = ascii8_out.sram_cs | ascii16_out.sram_cs | fm_pac_out.sram_cs | gm2_out.sram_cs;
