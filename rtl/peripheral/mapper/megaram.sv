e2134e52a7f11425f50944150ca8ecda2e0cfdeb
Molekula <tomas@dlabac.net>
2025-01-19 22:46:47 +0100

    Licence

--- rtl/peripheral/mapper/megaram.sv
+++ rtl/peripheral/mapper/megaram.sv
@@ -1,12 +1,50 @@
+// megaram.sv
+//
+// original megaram.vhd
+//   Mega-ROM emulation, ASC8K(8Mbits), ASC16K/SCC+(16Mbits)
+//   Revision 2.01
+//
+// Copyright (c) 2006 Kazuhiro Tsujikawa (ESE Artists' factory)
+// All rights reserved.
+//
+// Redistribution and use of this source code or any derivative works, are
+// permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Redistributions may not be sold, nor may they be used in a commercial
+//    product or activity without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//
+//
+//  modified by t.hara / KdL
+//
+// rewrite in Verilog and integrated MiserFpga project by Molekula 2024-2025
+//
+
 module mapper_megaram (
-    cpu_bus_if.device_mp     cpu_bus,        // Interface for CPU communication
-    mapper_out               out,            // Interface for mapper output
-    block_info               block_info,     // Struct containing mapper configuration and parameters
-    device_bus               device_out,      // Interface for device output 
+    cpu_bus_if.device_mp     cpu_bus,
+    mapper_out               out,
+    block_info               block_info,
+    device_bus               device_out,
     input                    ocm_slot1_mode,
     input              [1:0] ocm_slot2_mode
 );
-
     
     logic       SccBankL[2], SccBankM[2];
     logic [7:0] SccBank0[2], SccBank1[2], SccBank2[2], SccBank3[2], SccModeA[2], SccModeB[2];
