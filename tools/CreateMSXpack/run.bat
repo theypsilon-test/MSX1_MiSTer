6e134294580a05a3899d71f2915e5e69913e3f13
Molekula <tomas@dlabac.net>
2025-05-29 19:33:36 +0200

    doplnění do verze

--- tools/CreateMSXpack/run.bat
+++
@@ -1,4 +0,0 @@
-@REM py createDev.py
-@py createComp.py --xml-dir Computer_testy --output-dir MSX_test
-@REM ssh mist  "rm -rf /media/fat/games/MSX1/NEW/"
-scp -r MSX_test/* mist:/media/fat/games/MSX1/NEW/
